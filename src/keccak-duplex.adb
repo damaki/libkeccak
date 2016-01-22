eea52e9837dbbbd51df5e239eded4ffc75f426ab
Daniel King <damaki.gh@gmail.com>
2016-01-22 13:19:34 -0400

    Bump copyright year

--- src/keccak-duplex.adb
+++ src/keccak-duplex.adb
@@ -1,5 +1,5 @@
 -------------------------------------------------------------------------------
--- Copyright (c) 2015, Daniel King
+-- Copyright (c) 2016 Daniel King
 -- All rights reserved.
 --
 -- Redistribution and use in source and binary forms, with or without
